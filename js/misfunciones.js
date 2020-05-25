ccf5a21652be7d30866e8c3abe440f9b3d0bef71
Juan Diego <jdrosero7@gmail.com>
2020-05-25 14:09:42 -0500

    mi primer proyecto en materialize

---
+++ js/misfunciones.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    $(".dropdown-trigger").dropdown();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+      });
+      $(document).ready(function(){
+        $('.collapsible').collapsible();
+      });
+      $(document).ready(function(){
+        $('.tabs').tabs();
+      });
+      $(document).ready(function(){
+        $('.modal').modal();
+      });
+      $(document).ready(function(){
+        $('.sidenav').sidenav();
+      });
+});
